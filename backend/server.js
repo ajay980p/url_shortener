7b75ac38ec20a77bce0c516c55d3f5c766650963
ajay980p <ajay980p@gmail.com>
2025-04-16 17:47:34 +0530

    commit changes

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 })
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     db();
     console.log(`Server running on port ${PORT}`);
 });
